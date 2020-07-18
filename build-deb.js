a097b783455c513bcba5dd9f4e52c45fdeba582f
RuzikNF <ruslan.g2002@gmail.com>
2020-07-18 05:44:06 +0300

    🏘️ Debian packaging fixes

--- build-deb.js
+++ build-deb.js
@@ -7,46 +7,46 @@ const process = require("child_process");
 const configFile = path.join(__dirname, "deploy", "config.json");
 const appName = JSON.parse(fs.readFileSync(configFile, { encoding: 'utf8' })).appName;
 const buildFolder = path.join(__dirname, "deploy", "linux", "build", appName);
+const paths = [
+  './deb-struct/DEBIAN/',
+  './deb-struct/usr/bin/',
+  './deb-struct/usr/lib/',
+  './deb-struct/usr/share/applications/',
+];
 function cleanDirectories() {
-    console.log("Cleaning DEBIAN:");
-    console.log(process.execSync('rm -rf ./deb-struct/DEBIAN/*'));
-    console.log("Cleaning bin:");
-    console.log(process.execSync('rm -rf ./deb-struct/usr/bin/*'));
-    console.log("Cleaning lib:");
-    console.log(process.execSync('rm -rf ./deb-struct/usr/lib/*'));
-    console.log("Cleaning applications:");
-    console.log(process.execSync('rm -rf ./deb-struct/usr/share/applications/*'));
+  paths.forEach(dir => fs.mkdirSync(dir, {recursive: true}));
+  paths.forEach(dir => process.execSync(`rm -rf ${dir}*`));
 }
 function copyControlFile() {
-    console.log("Copying control:");
-    console.log(process.execSync('cp ./control ./deb-struct/DEBIAN/control'));
+  console.log("Copying control:");
+  console.log(process.execSync('cp ./control ./deb-struct/DEBIAN/control'));
 }
 function copyBuildFolderToLib() {
-    const folderPath = path.join(__dirname, "deb-struct", "usr", "lib");
-    console.log("Copying Build Folder:");
-    console.log(process.execSync('cp -R "' + buildFolder + '" "' + folderPath + '"'));
-    console.log(process.execSync('cp -R ./assets "./deb-struct/usr/lib/' + appName + '"'));
-    console.log(process.execSync('mv "./deb-struct/usr/lib/' + appName + '" ./deb-struct/usr/lib/' + appName.replace(' ', '').toLowerCase()));
+  const folderPath = path.join(__dirname, "deb-struct", "usr", "lib");
+  console.log("Copying Build Folder:");
+  console.log(process.execSync('cp -R "' + buildFolder + '" "' + folderPath + '"'));
+  console.log(process.execSync('cp -R ./assets "./deb-struct/usr/lib/' + appName + '"'));
+  console.log(process.execSync('mv "./deb-struct/usr/lib/' + appName + '" ./deb-struct/usr/lib/' + appName.replace(' ', '').toLowerCase()));
 }
 function createSymlinkToBin() {
-    const folderPath = '"' + path.join(__dirname, "deb-struct", "usr", "bin", appName) + '"';
-    console.log("Copying Symlink:");
-    console.log(process.execSync('ln -s /usr/lib/' + appName.replace(' ', '').toLowerCase() + '/qode ' + folderPath));
+  const folderPath = '"' + path.join(__dirname, "deb-struct", "usr", "bin", appName) + '"';
+  console.log("Copying Symlink:");
+  console.log(process.execSync('ln -s /usr/lib/' + appName.replace(' ', '').toLowerCase() + '/qode ' + folderPath));
 }
 function copyDesktopFileToApplications() {
-    console.log("Copying Desktop File:");
-    console.log(process.execSync('cp "' + path.join(buildFolder, getFilesFromPath(buildFolder, ".desktop")[0]) + '" ./deb-struct/usr/share/applications/' + appName.replace(' ', '').toLowerCase() + '.desktop'));
+  console.log("Copying Desktop File:");
+  console.log(process.execSync('cp "' + path.join(buildFolder, getFilesFromPath(buildFolder, ".desktop")[0]) + '" ./deb-struct/usr/share/applications/' + appName.replace(' ', '').toLowerCase() + '.desktop'));
 }
 function createDeb() {
-    // Create DEBIAN File
-    console.log("Generating Debian:");
-    console.log(process.execSync("dpkg-deb --build deb-struct"));
-    // Rename debian
-    console.log(process.execSync("mv deb-struct.deb " + appName.replace(' ', '').toLowerCase() + ".deb"));
+  // Create DEBIAN File
+  console.log("Generating Debian:");
+  console.log(process.execSync("dpkg-deb --build deb-struct"));
+  // Rename debian
+  console.log(process.execSync("mv deb-struct.deb " + appName.replace(' ', '').toLowerCase() + ".deb"));
 }
 function getFilesFromPath(path, extension) {
-    let files = fs.readdirSync(path);
-    return files.filter(file => file.match(new RegExp(`.*\.(${extension})`, 'ig')));
+  let files = fs.readdirSync(path);
+  return files.filter(file => file.match(new RegExp(`.*\.(${extension})`, 'ig')));
 }
 cleanDirectories();
 copyControlFile();
