58538f80612d869f328543acef267c34ac7b8dd0
RuzikNF <ruslan.g2002@gmail.com>
2020-07-30 19:38:09 +0300

    build(deb): remove useless debug info

--- build-deb.js
+++ build-deb.js
@@ -1,48 +1,49 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 const path = require("path");
 const fs = require("fs");
 const process = require("child_process");
-// Get the foldername
-const configFile = path.join(__dirname, "deploy", "config.json");
-const appName = JSON.parse(fs.readFileSync(configFile, { encoding: 'utf8' })).appName;
-const buildFolder = path.join(__dirname, "deploy", "linux", "build", appName);
+
+const CONFIG_FILE = path.join(__dirname, "deploy", "config.json");
+const { appName } = JSON.parse(fs.readFileSync(CONFIG_FILE, { encoding: 'utf8' }));
+const safeAppName = appName.replace(' ', '').toLowerCase();
+
+const BUILD_DIR = path.join(__dirname, "deploy", "linux", "build", appName);
+const LIB_DIR = path.join(__dirname, "deb-struct", "usr", "lib");
+const BIN_DIR = path.join(__dirname, "deb-struct", "usr", "bin", appName);
 const paths = [
   './deb-struct/DEBIAN/',
   './deb-struct/usr/bin/',
   './deb-struct/usr/lib/',
   './deb-struct/usr/share/applications/',
 ];
+
 function cleanDirectories() {
   paths.forEach(dir => fs.mkdirSync(dir, {recursive: true}));
   paths.forEach(dir => process.execSync(`rm -rf ${dir}*`));
 }
 function copyControlFile() {
-  console.log("Copying control:");
-  console.log(process.execSync('cp ./control ./deb-struct/DEBIAN/control'));
+  console.log("Copying control...");
+  process.execSync('cp ./control ./deb-struct/DEBIAN/control');
 }
 function copyBuildFolderToLib() {
-  const folderPath = path.join(__dirname, "deb-struct", "usr", "lib");
-  console.log("Copying Build Folder:");
-  console.log(process.execSync('cp -R "' + buildFolder + '" "' + folderPath + '"'));
-  console.log(process.execSync('cp -R ./assets "./deb-struct/usr/lib/' + appName + '"'));
-  console.log(process.execSync('mv "./deb-struct/usr/lib/' + appName + '" ./deb-struct/usr/lib/' + appName.replace(' ', '').toLowerCase()));
+  console.log("Copying build directory...");
+  process.execSync(`cp -R "${BUILD_DIR}" "${LIB_DIR}"`);
+  process.execSync(`cp -R ./assets "./deb-struct/usr/lib/${appName}"`);
+  process.execSync(`mv "./deb-struct/usr/lib/${appName}" ./deb-struct/usr/lib/${safeAppName}`);
 }
 function createSymlinkToBin() {
-  const folderPath = '"' + path.join(__dirname, "deb-struct", "usr", "bin", appName) + '"';
-  console.log("Copying Symlink:");
-  console.log(process.execSync('ln -s /usr/lib/' + appName.replace(' ', '').toLowerCase() + '/qode ' + folderPath));
+  console.log("Copying symlink...");
+  process.execSync(`ln -s /usr/lib/${safeAppName}/qode "${BIN_DIR}"`);
 }
 function copyDesktopFileToApplications() {
-  console.log("Copying Desktop File:");
-  console.log(process.execSync('cp "' + path.join(buildFolder, getFilesFromPath(buildFolder, ".desktop")[0]) + '" ./deb-struct/usr/share/applications/' + appName.replace(' ', '').toLowerCase() + '.desktop'));
+  console.log("Copying .desktop file...");
+  const from = path.join(BUILD_DIR, getFilesFromPath(BUILD_DIR, ".desktop")[0]);
+  const to = path.join(__dirname, 'deb-struct/usr/share/applications/', safeAppName + '.desktop')
+  process.execSync(`cp "${from}" "${to}"`);
 }
 function createDeb() {
-  // Create DEBIAN File
-  console.log("Generating Debian:");
-  console.log(process.execSync("dpkg-deb --build deb-struct"));
-  // Rename debian
-  console.log(process.execSync("mv deb-struct.deb " + appName.replace(' ', '').toLowerCase() + ".deb"));
+  console.log("Generating Debian package...");
+  process.execSync("dpkg-deb --build deb-struct");
+  process.execSync(`mv deb-struct.deb ${safeAppName}.deb`);
 }
 function getFilesFromPath(path, extension) {
   let files = fs.readdirSync(path);
@@ -54,4 +55,3 @@ copyBuildFolderToLib();
 createSymlinkToBin();
 copyDesktopFileToApplications();
 createDeb();
-//# sourceMappingURL=build-deb.js.map
