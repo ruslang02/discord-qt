edb47ce89807acf8022bcebd8bf38b55c291c39c
RuzikNF <ruslan.g2002@gmail.com>
2020-08-03 18:27:53 +0300

    feat: display user custom statuses and status bullet

--- src/components/UserButton/UserButton.ts
+++ src/components/UserButton/UserButton.ts
@@ -1,16 +1,20 @@
-import { QLabel, QWidget, QPixmap, QBoxLayout, Direction, WidgetEventTypes } from "@nodegui/nodegui";
+import { QLabel, QWidget, QPixmap, QBoxLayout, Direction, WidgetEventTypes, AlignmentFlag } from "@nodegui/nodegui";
 import { User, GuildMember } from "discord.js";
 import { pictureWorker } from "../../utilities/PictureWorker";
 import { DChannelButton } from '../DChannelButton/DChannelButton';
 import './UserButton.scss';
 import { CancelToken } from '../../utilities/CancelToken';
-import { app } from '../..';
+import { app, MAX_QSIZE } from '../..';
+import { PresenceStatusColor } from '../../structures/PresenceStatusColor';
 
 export class UserButton extends DChannelButton {
   private avatar = new QLabel(this);
-  private infoContainer = new QWidget(this);
-  private userNameLabel = new QLabel(this);
+  private nameLabel = new QLabel(this);
+  private statusInd = new QLabel(this);
+  private nameLayout = new QBoxLayout(Direction.LeftToRight);
+  private statusIcon = new QLabel(this);
   private statusLabel = new QLabel(this);
+  private statusLayout = new QBoxLayout(Direction.LeftToRight);
   private infoControls = new QBoxLayout(Direction.TopToBottom);
   private user?: User;
 
@@ -23,28 +27,37 @@ export class UserButton extends DChannelButton {
   }
 
   initComponent() {
-    const { avatar, infoContainer, userNameLabel, statusLabel, layout, infoControls } = this;
+    const { avatar, nameLabel, nameLayout, layout, infoControls, statusLayout, statusLabel, statusIcon, statusInd } = this;
 
     if (!app.config.enableAvatars) avatar.hide();
     avatar.setFixedSize(32, 32);
     avatar.setObjectName('Avatar');
     infoControls.setSpacing(0);
     infoControls.setContentsMargins(0, 0, 0, 0);
-    infoContainer.setLayout(infoControls);
-    userNameLabel.setObjectName('UserNameLabel');
-    userNameLabel.setMinimumSize(24, 0);
-    userNameLabel.setFlexNodeSizeControlled(false);
+    nameLabel.setObjectName('UserNameLabel');
+    nameLabel.setMinimumSize(24, 0);
+    nameLabel.setFlexNodeSizeControlled(false);
+    statusLabel.setAlignment(AlignmentFlag.AlignVCenter);
     statusLabel.setObjectName('StatusLabel');
-    const labels = [userNameLabel, statusLabel];
-    labels.forEach(w => infoControls.addWidget(w));
+    statusIcon.setMinimumSize(0, 0);
+    statusInd.setText('●');
+    nameLayout.setSpacing(6);
+    nameLayout.addWidget(nameLabel);
+    nameLayout.addWidget(statusInd, 1);
+    statusLayout.setSpacing(4);
+    statusLayout.addWidget(statusIcon);
+    statusLayout.addWidget(statusLabel, 1);
+
+    infoControls.addLayout(nameLayout);
+    infoControls.addLayout(statusLayout);
+
     layout.setSpacing(10);
     layout.addWidget(avatar, 0);
-    layout.addWidget(infoContainer, 1);
-    this.labels.push(...labels);
+    layout.addLayout(infoControls, 1);
+    this.labels = [nameLabel, statusLabel];
 
     this.addEventListener(WidgetEventTypes.HoverEnter, () => this.setHovered(true));
     this.addEventListener(WidgetEventTypes.HoverLeave, () => this.setHovered(false));
-    [avatar, infoContainer].forEach(w => this.layout?.addWidget(w));
   }
   private hasPixmap = false;
   async loadAvatar() {
@@ -63,10 +76,40 @@ export class UserButton extends DChannelButton {
   async loadUser(someone: User | GuildMember) {
     const user = someone instanceof GuildMember ? someone.user : someone;
     const member = someone instanceof GuildMember ? someone : null;
-
-    this.userNameLabel.setText(member?.nickname ?? user.username);
-    this.statusLabel.setText(user.presence.status);
-
+    if (!user) return;
     this.user = user;
+    const { presence } = user;
+
+    this.nameLabel.setText(member?.nickname ?? user.username);
+    this.statusInd.setInlineStyle(`color: ${PresenceStatusColor.get(presence.status)}`);
+    this.statusIcon.hide();
+
+    if (presence.activities.length) {
+      const activity = presence.activities[0];
+      [this.statusLabel, this.statusIcon].forEach(w => w.setMaximumSize(MAX_QSIZE, MAX_QSIZE));
+      let status = "";
+      switch (activity.type) {
+        case 'CUSTOM_STATUS':
+          status = activity.state || '';
+          if (!activity.emoji) break;
+          if (activity.emoji.name && !activity.emoji.id) {
+            this.statusIcon.setText(activity.emoji.name);
+            this.statusIcon.show();
+            break;
+          }
+          // @ts-ignore
+          const emojiUrl = app.client.rest.cdn.Emoji(activity.emoji.id, 'png');
+          if (!emojiUrl) break;
+          const buf = await pictureWorker.loadImage(emojiUrl, { roundify: false, size: 16 })
+          if (!buf) break;
+          const pix = new QPixmap();
+          pix.loadFromData(buf, 'PNG');
+          this.statusIcon.setPixmap(pix);
+          this.statusIcon.show();
+      }
+      this.statusLabel.setText(status);
+    } else {
+      [this.statusLabel, this.statusIcon].forEach(w => w.setMaximumSize(MAX_QSIZE, 0));
+    }
   }
 }
