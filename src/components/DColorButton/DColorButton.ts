90066c6d0347b10ffbc6e90722e6643a860e6967
RuzikNF <ruslan.g2002@gmail.com>
2020-08-05 17:57:30 +0300

    fix: more colors to ColorButton, global fancy scrollbars

--- src/components/DColorButton/DColorButton.ts
+++ src/components/DColorButton/DColorButton.ts
@@ -5,6 +5,7 @@ export enum DColorButtonColor {
   BLURPLE = 0,
   RED = 1,
   RED_TEXT = 2,
+  WHITE_TEXT = 3,
 }
 export class DColorButton extends QPushButton {
   constructor(color = DColorButtonColor.BLURPLE) {
