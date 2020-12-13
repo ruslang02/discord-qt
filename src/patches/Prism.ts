d653a459c6d375fdfe4471e5ad0c98099d303401
Garifullin Ruslan <ruslan.g2002@gmail.com>
2020-12-13 04:38:34 +0300

    perf(Voice): switch to @djs/opus native module

--- src/patches/Prism.ts
+++ src/patches/Prism.ts
@@ -1,9 +1,8 @@
-import opusscript from 'opusscript';
-
 const Loader = require('prism-media/src/util/loader');
+const Opus = require('@discordjs/opus/build-tmp-napi-v3/Release/opus.node');
 
 try {
   Loader.require = function load() {
-    return { Encoder: opusscript };
+    return { Encoder: Opus.OpusEncoder };
   };
 } catch (e) {}
--- webpack.config.js
+++ webpack.config.js
@@ -20,9 +20,9 @@ try {
 
 module.exports = (_env, argv) => {
   const isDev = argv.mode !== 'production';
-  const themes = Object.fromEntries(readdirSync('./src/themes').map(value => [
+  const themes = Object.fromEntries(readdirSync('./src/themes').map((value) => [
     value.replace('.scss', ''),
-    `./src/themes/${value}`
+    `./src/themes/${value}`,
   ]));
 
   return {
@@ -30,14 +30,14 @@ module.exports = (_env, argv) => {
     entry: {
       'index.js': './src',
       'worker.js': './worker',
-      ...themes
+      ...themes,
     },
     optimization: {
       minimize: !isDev,
       minimizer: [new TerserPlugin({
         terserOptions: {
-          keep_classnames: true
-        }
+          keep_classnames: true,
+        },
       })],
     },
     target: 'node',
@@ -65,9 +65,9 @@ module.exports = (_env, argv) => {
                 transpileOnly: true,
                 happyPackMode: true,
                 experimentalWatchApi: true,
-              }
-            }
-          ]
+              },
+            },
+          ],
         },
         {
           test: /\.(png|jpe?g|gif|svg)$/i,
@@ -90,8 +90,8 @@ module.exports = (_env, argv) => {
               options: {
                 sassOptions: {
                   importer: globImporter(),
-                }
-              }
+                },
+              },
             },
           ],
         },
@@ -111,7 +111,7 @@ module.exports = (_env, argv) => {
       mainFields: ['main'],
       alias: {
         'fetch': path.join(__dirname, '../node_modules', 'whatwg-fetch', 'fetch.js'),
-      }
+      },
     },
     plugins: [
       new CleanWebpackPlugin(),
@@ -123,11 +123,10 @@ module.exports = (_env, argv) => {
         patterns: [
           { from: 'assets', to: 'assets' },
           { from: 'src/locales', to: 'locales' },
-          { from: 'node_modules/opusscript/build/opusscript_native_wasm.wasm' },
           { from: 'node_modules/ffmpeg-static/ffmpeg', noErrorOnMissing: true },
-          { from: 'node_modules/ffmpeg-static/ffmpeg.exe', noErrorOnMissing: true }
-        ]
-      })
+          { from: 'node_modules/ffmpeg-static/ffmpeg.exe', noErrorOnMissing: true },
+        ],
+      }),
     ],
   }
 };
