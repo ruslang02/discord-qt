96caa1bc41747aace2759a9c3002095e9ec8be9b
Garifullin Ruslan <ruslan.g2002@gmail.com>
2020-12-13 16:37:52 +0300

    revert(Voice): revert @djs/opus commit due to cross-platform
    This reverts commit d653a459c6d375fdfe4471e5ad0c98099d303401.

--- src/patches/Prism.ts
+++ src/patches/Prism.ts
@@ -1,8 +1,9 @@
+import opusscript from 'opusscript';
+
 const Loader = require('prism-media/src/util/loader');
-const Opus = require('@discordjs/opus/build-tmp-napi-v3/Release/opus.node');
 
 try {
   Loader.require = function load() {
-    return { Encoder: Opus.OpusEncoder };
+    return { Encoder: opusscript };
   };
 } catch (e) {}
